6283720d63588af153cc8606227ffcf748a087ab
Alex Trexler <alextrex@udel.edu>
2024-03-07 18:23:31 -0500

    added top down movement

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,28 +1,72 @@
 import Phaser from "phaser";
-import PhaserLogo from "../objects/phaserLogo";
-import FpsText from "../objects/fpsText";
 
 export default class MainScene extends Phaser.Scene {
-    fpsText: FpsText;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "MainScene" });
     }
 
     create() {
-        new PhaserLogo(this, this.cameras.main.width / 2, 0);
-        this.fpsText = new FpsText(this);
+        this.player = this.physics.add.sprite(100, 450, "player");
+        this.player.setCollideWorldBounds(true);
+        this.player.setScale(1.5);
 
-        const message = `Phaser v${Phaser.VERSION}`;
-        this.add
-            .text(this.cameras.main.width - 15, 15, message, {
-                color: "#000000",
-                fontSize: "24px",
-            })
-            .setOrigin(1, 0);
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("player", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "player", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("player", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
     }
 
     update() {
-        this.fpsText.update();
+        if (!this.cursors) {
+            return;
+        }
+
+        if (!this.cursors.left.isDown && !this.cursors.right.isDown) {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
+
+        if (!this.cursors.up.isDown && !this.cursors.down.isDown) {
+            this.player?.setVelocityY(0);
+        }
+
+        if (this.cursors.left.isDown && !this.cursors.right.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        } else if (this.cursors.right.isDown && !this.cursors.left.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        }
+        if (this.cursors.up.isDown && !this.cursors.down.isDown) {
+            this.player?.setVelocityY(-160);
+        } else if (this.cursors.down.isDown && !this.cursors.up.isDown) {
+            this.player?.setVelocityY(160);
+        }
     }
 }
